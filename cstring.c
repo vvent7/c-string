647b166b93ef325b7bc788741dd6f4d7a879486b
Fernando Ventilari <fernandoventilari@gmail.com>
2021-03-01 22:25:47 -0400

    cstring lib v1 done

--- cstring.c
+++ cstring.c
@@ -1,104 +1,213 @@
 #include<stdlib.h>
+#include<string.h>
 #include "cstring.h"
 
-unsigned short string_valid(String str){
-  return (str!=NULL && (*str)!=NULL);
+unsigned short string_valid(const String str){
+  return (str!=NULL) ? 1 : 0;
 }
 
-StringData* string_data(String str){
-  return string_valid(str) ? ((StringData*)(*str))-1 : NULL;
+StringData* string_data(const String str){
+  return string_valid(str) ? ((StringData*)(str))-1 : NULL;
+}
+
+StringData* string_ptr_data(const String *str){
+  return str ? string_data(*str) : NULL;
 }
 
 String string_new_cfg(unsigned short auto_shrink){
   StringData *sd = (StringData*) malloc(sizeof(StringData) + sizeof(char));
-  String str = (String) malloc(sizeof(char*));
+  String str = (String) (sd+1);
 
   sd->sz = sd->cap = 0; sd->auto_shrink = auto_shrink ? 1 : 0;
-  (*str)[0] = '\0';
+  (str)[0] = '\0';
 
   return str;
 }
 
-char* string_begin(String str){
-  return (*str);
+String* string_new_ptr_cfg(unsigned short auto_shrink){
+  String *pstr = (String*) malloc(sizeof(String*));
+  (*pstr) = string_new_cfg(auto_shrink);
+  return pstr;
 }
 
-char* string_end(String str){
-  return string_valid(str) ? (*str)+(string_data(str))->sz : NULL;
+char* string_begin(const String str){
+  return (str);
 }
 
-size_t string_size(String str){
+char* string_end(const String str){
+  return string_valid(str) ? str + (string_data(str))->sz : NULL;
+}
+
+size_t string_size(const String str){
   StringData *sd = string_data(str);
   return sd ? sd->sz : (size_t)0;
 }
 
-size_t string_capacity(String str){
+size_t string_size_by_type(const char *str, const StrType sType){
+  if(str==NULL) return 0;
+  switch(sType){
+    case CSTRING_STRING_TYPE: return string_size((String) str);
+    case CSTRING_ARR_TYPE: return strlen(str);
+    default: return 0;
+  }
+}
+
+size_t string_capacity(const String str){
   StringData *sd = string_data(str);
   return sd ? sd->cap : (size_t)0;
 }
 
-unsigned short string_empty(String str){
+unsigned short string_empty(const String str){
   return (string_size(str)==0);
 }
 
-char* string_set_size(String str, size_t new_size){
-  StringData *sd = string_data(str);
+String string_set_size(String *str, const size_t new_size){
+  StringData *sd = string_ptr_data(str);
 
-  if(sd==NULL) return NULL;
-
-  new_size = MIN(new_size, string_npos-1);
+  if(sd==NULL || new_size>=string_npos) return NULL;
   
   if(new_size!=sd->sz){
     if(new_size > sd->cap){
       size_t new_cap = MAX(sd->cap, 1);
-      while(new_size > new_cap) new_cap *= _STRING_GROWTH_FACTOR;
+      while(new_size > new_cap) new_cap *= _CSTRING_GROWTH_FACTOR;
       sd = ((StringData*) string_set_capacity(str, new_cap)) - 1;
-      sd->sz = new_size;
     }
-    sd->sz = new_size; str[new_size] = '\0';
+    (*str)[sd->sz] = ' '; (*str)[sd->sz = new_size] = '\0';
     if(new_size < sd->cap && sd->auto_shrink)
       sd = ((StringData*) string_shrink_to_fit(str)) - 1;
   }
 
-  return (char*) (sd+1);
+  return (*str);
 }
 
-char* string_set_capacity(String str, size_t new_cap){
-  StringData *sd = string_data(str);
+String string_set_capacity(String *str, const size_t new_cap){
+  StringData *sd = string_ptr_data(str);
 
-  if(sd==NULL) return NULL;
-
-  new_cap = MIN(new_cap, string_npos-1);
+  if(sd==NULL || new_cap>=string_npos) return NULL;
 
   if(new_cap!=sd->cap){
     sd = realloc(sd, sizeof(StringData) + sizeof(char) * (new_cap + 1));
-    sd->cap = new_cap; sd->sz = MIN(sd->cap, sd->sz);
     (*str) = (String) (sd+1);
-    (*str)[new_cap] = '\0';
+
+    if(sd->cap < new_cap) (*str)[sd->cap] = ' ';
+    (*str)[sd->cap = new_cap] = '\0';
+    sd->sz = MIN(sd->cap, sd->sz);
   }
 
-  return (char*) (sd+1);
+  return (*str);
 }
 
-char* string_shrink_to_fit(String str){
-  StringData *sd = string_data(str);
+void string_set_auto_shrink(String *str, const unsigned short auto_shrink){
+  StringData *sd = string_ptr_data(str);
+  if(sd && (sd->auto_shrink = auto_shrink ? 1 : 0))
+    string_shrink_to_fit(str);
+}
+
+String string_shrink_to_fit(String *str){
+  StringData *sd = string_ptr_data(str);
+  size_t new_cap;
 
   if(sd==NULL) return NULL;
-
-  size_t new_cap = sd->cap;
-  while(sd->sz < new_cap) new_cap /= _STRING_GROWTH_FACTOR;
-  return string_set_capacity(str, new_cap*_STRING_GROWTH_FACTOR);
+  
+  new_cap = sd->cap;
+  while(sd->sz < new_cap) new_cap /= _CSTRING_GROWTH_FACTOR;
+  return string_set_capacity(str, new_cap*_CSTRING_GROWTH_FACTOR);
 }
 
-char string_at(String str, size_t index){
-  return (*str)[index];
+char string_at(const String str, const size_t index){
+  return index < string_size(str) ? str[index] : '\0';
 }
 
-char string_front(String str){
-  return string_empty(str) ? 0 : (*str)[0];
+char string_front(const String str){
+  return string_empty(str) ? '\0' : str[0];
 }
 
-char string_back(String str){
+char string_back(const String str){
   size_t sz;
-  return (sz = string_size(str))==0 ? 0 : (*str)[sz-1];
+  return (sz = string_size(str))==0 ? '\0' : str[sz-1];
+}
+
+char* string_set_gap(String *str, const size_t index, const size_t length){
+  StringData *sd = string_ptr_data(str);
+  size_t to_move;
+  char *begin, *end;
+
+  if(sd==NULL || index>sd->sz) return NULL;
+
+  to_move = (sd->sz - index) * sizeof(char);
+  string_set_size(str, sd->sz+length);
+  begin = (*str) + index; end = begin + length;
+  
+  memmove(end, begin, to_move);
+
+  return begin;
+}
+
+String string_n_insert(String *s1, const size_t index, const char *s2, const size_t length){
+  StringData *sd = s1 ? string_data(*s1) : NULL;
+  char *temp, *dest;
+
+  if(sd==NULL || index>sd->sz || s2==NULL) return NULL;
+
+  temp = malloc(sizeof(char)*(length+1)); strncpy(temp, s2, length);
+  dest = string_set_gap(s1, index, length); strncpy(dest, temp, length); 
+  
+  free(temp);
+
+  return (*s1);
+}
+
+String string_insert(String *s1, const size_t index, const char *s2, const StrType s2Type){
+  return string_n_insert(s1, index, s2, string_size_by_type(s2, s2Type));
+}
+
+String string_n_append(String *s1, const char *s2, const size_t length){
+  return s1 ? string_n_insert(s1, string_size(*s1), s2, length) : NULL;
+}
+
+String string_append(String *s1, const char *s2, const StrType s2Type){
+  return string_n_append(s1, s2, string_size_by_type(s2, s2Type));
+}
+
+String string_insert_c(String *str, const size_t index, const char c){
+  char *dest;
+  if((dest = string_set_gap(str, index, 1))==NULL) return NULL;
+  (*dest) = c;
+  return (*str);
+}
+
+String string_append_c(String *str, const char c){
+  return str ? string_insert_c(str, string_size(*str), c) : NULL;
+}
+
+String string_n_erase(String *str, const size_t index, size_t length){
+  StringData *sd = string_ptr_data(str);
+  char *begin, *end;
+
+  if(sd==NULL || index>=sd->sz) return NULL;
+
+  length = MIN(length, sd->sz-index);
+  begin = (*str) + index; end = begin + length;
+
+  memmove(begin, end, (sd->sz-index-length) * sizeof(char));
+  string_set_size(str, sd->sz-length);
+
+  return (*str);
+}
+
+String string_erase(String *str, const size_t index){
+  return string_n_erase(str, index, 1);
+}
+
+String string_clear(String *str){
+  return string_set_size(str, 0);
+}
+
+void string_free(String str){
+  free(string_data(str));
+}
+
+void string_ptr_free(String *str){
+  free(string_ptr_data(str));
+  free(str);
 }
--- cstring.h
+++ cstring.h
@@ -3,7 +3,8 @@
 
 #include <stdlib.h>
 
-#define _STRING_GROWTH_FACTOR 2
+#define _CSTRING_GROWTH_FACTOR 2
+#define _CSTRING_DEFAULT_AUTO_SHRINK 0
 
 #ifndef MIN
   #define MIN(x, y) (x<y ? x : y) /* Returns the minimum number (x or y). */
@@ -13,8 +14,9 @@
   #define MAX(x, y) (x>y ? x : y) /* Returns the maximum number (x or y). */
 #endif
 
-typedef char** String;
+typedef char* String;
 
+typedef enum{CSTRING_STRING_TYPE, CSTRING_ARR_TYPE} StrType;
 typedef struct{
   size_t sz, cap; /* size, capacity */
   unsigned short auto_shrink; /* automatic shrink on(1) / off(0) */
@@ -24,60 +26,130 @@ static const size_t string_npos = -1;
 
 /*----------------------------------Auxiliaries----------------------------------*/
 /* Verifies whether the String is valid */
-unsigned short string_valid(String str);
+unsigned short string_valid(const String str);
 
 /* Returns the String's Data Address */
-StringData* string_data(String str);
+StringData* string_data(const String str);
 /*==============================================================================*/
 
 /*----------------------------------Constructor----------------------------------*/
 /* Creates the String and returns its address */
 String string_new_cfg(unsigned short auto_shrink);
-#define string_new() (string_new_cfg(0))
+String* string_new_ptr_cfg(unsigned short auto_shrink);
+#define string_new() (string_new_cfg(_CSTRING_DEFAULT_AUTO_SHRINK))
+#define string_new_ptr() (string_new_ptr_cfg(_CSTRING_DEFAULT_AUTO_SHRINK))
 /*==============================================================================*/
 
 /*-----------------------------------Pointers-----------------------------------*/
-/* Returns pointer to the beginning of String */
-char* string_begin(String str);
+/* Returns char pointer to the beginning of String */
+char* string_begin(const String str);
 
-/* Returns pointer to the end of String */
-char* string_end(String str);
+/* Returns char pointer to the end of String */
+char* string_end(const String str);
 /*==============================================================================*/
 
 /*----------------------------------Dimensions----------------------------------*/
 /* Returns String's size */
-size_t string_size(String str);
+size_t string_size(const String str);
+
+/* Returns String's size depending on sType.
+ * sType: specifies type of 'str':
+ *   - CSTRING_STRING_TYPE -> String (uses stored size / O(1))
+ *   - CSTRING_ARR_TYPE -> char* (uses strlen to find size / O(n))*/
+size_t string_size_by_type(const char *str, const StrType sType);
 
 /* Returns String's Total Capacity */
-size_t string_capacity(String str);
+size_t string_capacity(const String str);
 
 /* Verifies whether the String is empty */
-unsigned short string_empty(String str);
+unsigned short string_empty(const String str);
 
 /* Sets String's Size
- * Returns: Pointer to the beginning of String */
-char* string_set_size(String str, size_t new_size);
+ * Returns: updated string */
+String string_set_size(String *str, const size_t new_size);
 
 /* Sets String's Capacity
- * Returns: Pointer to the beginning of String's array */
-char* string_set_capacity(String str, size_t new_cap);
+ * Returns: updated string */
+String string_set_capacity(String *str, const size_t new_cap);
+
+/* Sets String's auto_shrink */
+void string_set_auto_shrink(String *str, const unsigned short auto_shrink);
 
 /* Requests the container to reduce its capacity to fit its size. 
  * Shrinks in such a way that the following statement is true: 
- * (size<=capacity && size>=(capacity/_STRING_GROWTH_FACTOR))
- * Returns: Pointer to the beginning of String's array */
-char* string_shrink_to_fit(String str);
+ * (size<=capacity && size>=(capacity/_CSTRING_GROWTH_FACTOR))
+ * Returns: updated string */
+String string_shrink_to_fit(String *str);
+
 /*==============================================================================*/
 
 /*--------------------------------Element Access--------------------------------*/
-/* Returns a reference to the element at position n in the vector. */
-char string_at(String str, size_t index);
+/* Returns the element at a specific 'index' in the vector, or '\0', if invalid */
+char string_at(const String str, const size_t index);
 
-/* Returns the first element in the vector, or 0 if it's empty */
-char string_front(String str);
+/* Returns the first char in the vector, or '\0' if it's empty */
+char string_front(const String str);
 
-/* Returns the last element in the vector, or 0 if it's empty */
-char string_back(String str);
+/* Returns the last char in the vector, or '\0' if it's empty */
+char string_back(const String str);
 /*==============================================================================*/
+
+/*-----------------------------------Modifiers-----------------------------------*/
+/* Sets a gap starting at a specific 'index' with a specific 'length' in the vector.
+ * Returns: char pointer to the beginning of gap in the array of the String */
+char* string_set_gap(String *str, const size_t index, const size_t length);
+
+/* Inserts 'length' elements of 's2' inside 's1' at specific 'index'
+ * Returns: updated string */
+String string_n_insert(String *s1, const size_t index, const char *s2, const size_t length);
+
+/* Inserts ALL elements of 's2' inside 's1' at specific 'index'
+ * s2Type: specifies type of 's2':
+ *   - CSTRING_STRING_TYPE -> String (uses stored size / O(1))
+ *   - CSTRING_ARR_TYPE -> char* (uses strlen to find size / O(n))
+ * Returns: updated string */
+String string_insert(String *s1, const size_t index, const char *s2, const StrType s2Type);
+
+/* Appends 'length' elements of 's2' at the end of 's1' 
+ * Returns: updated string */
+String string_n_append(String *s1, const char *s2, const size_t length);
+
+/* Inserts ALL elements of 's2' inside 's1' at the end of 's1'
+ * s2Type: specifies type of 's2':
+ *   - CSTRING_STRING_TYPE -> String (uses stored size / O(1))
+ *   - CSTRING_ARR_TYPE -> char* (uses strlen to find size / O(n))
+ * Returns: updated string */
+String string_append(String *s1, const char *s2, const StrType s2Type);
+
+/* Inserts char 'c' inside 's1' at specific 'index'
+ * Returns: updated string */
+String string_insert_c(String *str, const size_t index, const char c);
+
+/* Appends char 'c' at the end of 's1' 
+ * Returns: updated string */
+String string_append_c(String *str, const char c);
+
+/* Removes 'length' elements from 'str' starting at a specific index
+ * Returns: 
+ * - Successful: updated string
+ * - Error: NULL */
+String string_n_erase(String *str, const size_t index, size_t length);
+
+/* Removes a single element from 'str' at specific index
+ * Returns: 
+ * - Successful: updated string
+ * - Error: NULL */
+String string_erase(String *str, const size_t index);
+
+/* Clears String's content */
+String string_clear(String *str);
+
+/* Frees all memory associated to the string */
+void string_free(String str);
+
+/* Frees all memory associated to the string and its pointer */
+void string_ptr_free(String *str);
+
+/*===============================================================================*/
 
 #endif
